18273fe11e07e103cc7446232d2564014a80d035
Reeve <reeveblake@gmail.com>
2019-10-25 19:40:11 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,22 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import java.net.URI
+import kotlin.collections.mutableListOf
 
 plugins {
 	kotlin("jvm") version "1.3.50"
-	application
+	id("com.github.johnrengelman.shadow") version "5.1.0"
+	java
 }
 
 group = "dev.reeve"
 version = "1.0-SNAPSHOT"
 
-application {
-
-	mainClassName = "dev.reeve.discordintegration.MainAppKt"
-}
 
 dependencies {
-	implementation(kotlin("stdlib-jdk8"))
-	implementation("no.tornado:tornadofx:1.7.17")
+	compile(kotlin("stdlib-jdk8"))
+	compile("no.tornado:tornadofx:1.7.17")
 	compile("club.minnced:java-discord-rpc:2.0.2")
+	compileClasspath("com.github.jengelman.gradle.plugins:shadow:5.1.0")
 }
 
 repositories {
@@ -36,4 +35,17 @@ compileKotlin.kotlinOptions {
 val compileTestKotlin: KotlinCompile by tasks
 compileTestKotlin.kotlinOptions {
 	jvmTarget = "1.8"
+}
+
+tasks {
+	jar {
+		classifier = null
+		manifest {
+			attributes["Main-Class"] = "dev.reeve.discordrpctool.MainAppKt"
+		}
+	}
+	shadowJar {
+		minimize()
+		configurations = mutableListOf(project.configurations.compile.get())
+	}
 }
